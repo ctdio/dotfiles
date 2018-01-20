c567cbb032e9e33ac984780909c38ac6630719c0
Charlie Duong <charlieduong94@gmail.com>
2018-01-20 13:14:53 -0500

    Add install script

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "Installing..."
+
+DOTFILES_DIR="$(cd $(dirname "$BASH_SOURCE[0]") && pwd)"
+
+echo "Installing various tools..."
+brew tap crisidev/homebrew-chunkwm
+
+brew install \
+  zsh \
+  tmux \
+  koekeishiya/formulae/skhd
+
+brew install --HEAD chunkwm
+
+brew services start skhd
+brew services start chunkwm
+
+echo "Fetching antigen..."
+curl -L git.io/antigen > antigen.zsh
+
+echo "Linking dotfiles..."
+# iterate through all files
+# and symlink them to home
+for filename in $(ls -A); do
+  if [ ${filename} != ".git" ]; then
+    echo "Linking ${filename} to ~/${filename}"
+    ln -nsf ${DOTFILES_DIR}/${filename} ~/${filename}
+  fi
+done
+
+echo "Install complete!"
