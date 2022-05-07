8e10d9f7db455b579691369d30379d29a5dbdeb5
Charlie Duong <charlieduong94@gmail.com>
2022-05-07 10:45:07 -0400

    Tweak startup script to load yabai scription addition on MacOS

--- startup.sh
+++ startup.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-# set key repeat
-xset r rate 220 60
+if [[ "$(uname)" = 'Linux' ]]; then
+  echo "Detected Linux"
+  echo "Setting key repeat and delay rate"
+  xset r rate 220 60
+elif [[ "$(uname)" = 'Darwin' ]]; then
+  echo "Detected Linux"
+  echo "Loading yabai scripting edition..."
+  sudo yabai --load-sa
+fi
+
+echo "Completed startup!"
