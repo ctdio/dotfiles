9cd9ea3c8d509897ef625ba2a42913263399f82f
Charlie Duong <charlieduong94@gmail.com>
2022-10-01 10:41:44 -0400

    Add gopls to lsp server list

--- nvim/lua/config/completion.lua
+++ nvim/lua/config/completion.lua
@@ -162,7 +162,7 @@ local function setup()
 
   -- Use a loop to conveniently call 'setup' on multiple servers and
   -- map buffer local keybindings when the language server attaches
-  local servers = { "tsserver", "astro", "rust_analyzer" }
+  local servers = { "tsserver", "astro", "rust_analyzer", "gopls" }
   for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup {
       on_attach = on_attach,
