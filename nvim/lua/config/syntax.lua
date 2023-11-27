31e58bfe929d53f8844d4c644e363c3a094d88fe
Charlie Duong <charlieduong94@gmail.com>
2023-11-27 18:11:56 -0500

    feat: add gitsigns textobject movement

--- nvim/lua/config/syntax.lua
+++ nvim/lua/config/syntax.lua
@@ -117,7 +117,7 @@ local function setup()
           ["]C"] = "@call.outer",
 
           -- assignment
-          ["]R"] = "@assignment.rhs",
+          ["]+"] = "@assignment.outer",
 
           -- loops
           ["]L"] = "@loop.outer",
@@ -164,6 +164,9 @@ local function setup()
           -- classes
           ["[C"] = "@call.outer",
 
+          -- assignment
+          ["[+"] = "@assignment.outer",
+
           -- loops
           ["[L"] = "@loop.outer",
 
@@ -176,30 +179,35 @@ local function setup()
       },
     },
   })
+
   local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
 
-  -- Repeat movement with ; and ,
-  -- ensure ; goes forward and , goes backward regardless of the last direction
-  --[[
-     [vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
-     [vim.keymap.set(
-     [  { "n", "x", "o" },
-     [  ",",
-     [  ts_repeat_move.repeat_last_move_previous
-     [)
-     ]]
   vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
   vim.keymap.set(
     { "n", "x", "o" },
     ",",
     ts_repeat_move.repeat_last_move_opposite
   )
+  -- make builtin f, F, t, T also repeatable with ; and ,
+  vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
+  vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
+  vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
+  vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
 
   vim.filetype.add({
     extension = {
       tf = "hcl",
     },
   })
+
+  -- add keybinds for gitsigns
+  local gs = require("gitsigns")
+
+  local next_hunk_repeat, prev_hunk_repeat =
+    ts_repeat_move.make_repeatable_move_pair(gs.next_hunk, gs.prev_hunk)
+
+  vim.keymap.set({ "n", "x", "o" }, "]h", next_hunk_repeat)
+  vim.keymap.set({ "n", "x", "o" }, "[h", prev_hunk_repeat)
 end
 
 return {
