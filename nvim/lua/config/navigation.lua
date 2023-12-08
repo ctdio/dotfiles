513cab26761e912eb4ea4017dbbee919b72a5a8f
Charlie Duong <charlieduong94@gmail.com>
2023-12-08 16:01:37 -0500

    feat: adjust keybinds for harpoon and tmux-sessionizer

--- nvim/lua/config/navigation.lua
+++ nvim/lua/config/navigation.lua
@@ -331,7 +331,7 @@ local function setup()
   vim.keymap.set("n", "<leader>a", function()
     harpoon:list():append()
   end)
-  vim.keymap.set("n", "<C-s>", function()
+  vim.keymap.set("n", "<C-t>", function()
     harpoon.ui:toggle_quick_menu(harpoon:list())
   end)
 
