989878d6fb7a696cd829479f3df31c4880b5faf3
Charlie Duong <charlieduong94@gmail.com>
2023-12-10 09:29:20 -0500

    feat: make neotest work with js monorepo

--- nvim/lua/config/test.lua
+++ nvim/lua/config/test.lua
@@ -5,6 +5,21 @@ local function setup()
       require("neotest-python"),
       require("neotest-jest")({
         jestCommand = "npx jest",
+        jestConfigFile = function()
+          local file = vim.fn.expand("%:p")
+          if string.find(file, "/packages/") then
+            return string.match(file, "(.-/[^/]+/)src") .. "jest.config.ts"
+          end
+
+          return vim.fn.getcwd() .. "/jest.config.ts"
+        end,
+        cwd = function()
+          local file = vim.fn.expand("%:p")
+          if string.find(file, "/packages/") then
+            return string.match(file, "(.-/[^/]+/)src")
+          end
+          return vim.fn.getcwd()
+        end,
       }),
       require("neotest-vitest"),
       require("neotest-rust"),
