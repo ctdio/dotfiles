1b3c0255eb1753b8cad114af2f2dd9459c81d6f0
Charlie Duong <charlieduong94@gmail.com>
2024-03-02 18:35:54 -0500

    feat: update sketchybar config

--- sketchybar/plugins/wifi.sh
+++ sketchybar/plugins/wifi.sh
@@ -3,6 +3,12 @@
 # The wifi_change event supplies a $INFO variable in which the current SSID
 # is passed to the script.
 
-WIFI=${INFO:-"Not Connected"}
+CURRENT_WIFI="$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I)"
+SSID="$(echo "$CURRENT_WIFI" | grep -o "SSID: .*" | sed 's/^SSID: //')"
+CURR_TX="$(echo "$CURRENT_WIFI" | grep -o "lastTxRate: .*" | sed 's/^lastTxRate: //')"
 
-sketchybar --set $NAME label="${WIFI}"
+if [ "$SSID" = "" ]; then
+  sketchybar --set $NAME label="Disconnected" icon=睊
+else
+  sketchybar --set $NAME label="$SSID (${CURR_TX}Mbps)" icon=直
+fi
