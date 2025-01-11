22e92862641b0254549a82bec999d5ec4bc3c551
Charlie Duong <charlie.duong@hey.com>
2025-01-10 19:57:01 -0500

    hack: periodically restart supermaven

--- nvim/lua/config/ai.lua
+++ nvim/lua/config/ai.lua
@@ -11,6 +11,29 @@ local function setup()
     end,
   })
 
+  local api = require("supermaven-nvim.api")
+
+  -- Supermaven has been dying occasionally
+  -- Set up periodic Supermaven restart
+  vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+      if not api.is_running() then
+        vim.cmd("SupermavenStart")
+      end
+      -- Set up timer for periodic restart
+      local timer = vim.loop.new_timer()
+      timer:start(
+        10 * 60 * 1000, -- 10 minutes in milliseconds
+        10 * 60 * 1000, -- 10 minutes in milliseconds
+        vim.schedule_wrap(function()
+          if not api.is_running() then
+            vim.cmd("SupermavenRestart")
+          end
+        end)
+      )
+    end,
+  })
+
   require("avante_lib").load()
 
   require("avante").setup({
