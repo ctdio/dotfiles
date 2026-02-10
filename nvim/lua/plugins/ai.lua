cc3f9883b044823551111e2d9195abab64610bc5
Charlie Duong <charlie.duong@hey.com>
2026-02-10 12:45:33 -0500

    chore: temp disable gh copilot

--- nvim/lua/plugins/ai.lua
+++ nvim/lua/plugins/ai.lua
@@ -3,48 +3,7 @@
 return {
   {
     "zbirenbaum/copilot.lua",
-    event = "VeryLazy",
-    config = function()
-      require("copilot").setup({
-        suggestion = {
-          enabled = true,
-          auto_trigger = true,
-          hide_during_completion = true,
-          debounce = 75,
-          keymap = {
-            accept = "<C-k>",
-            accept_word = false,
-            accept_line = false,
-            next = "<M-]>",
-            prev = "<M-[>",
-            dismiss = "<C-]>",
-          },
-        },
-        panel = {
-          enabled = true,
-          auto_refresh = false,
-          keymap = {
-            jump_prev = "[[",
-            jump_next = "]]",
-            accept = "<CR>",
-            refresh = "gr",
-            open = "<M-CR>",
-          },
-          layout = { position = "bottom", ratio = 0.4 },
-        },
-        filetypes = {
-          yaml = false,
-          markdown = false,
-          help = false,
-          gitcommit = false,
-          gitrebase = false,
-          hgcommit = false,
-          svn = false,
-          cvs = false,
-          ["."] = false,
-        },
-      })
-    end,
+    enabled = false,
   },
 
   {
